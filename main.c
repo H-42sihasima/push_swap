72fd199dfa0ea35f832d0e223db6587817f0f074
sihasima <sihasima@student.42antananarivo.mg>
2026-04-14 17:20:40 +0300

    update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sihasima <sihasima@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 15:49:20 by sihasima          #+#    #+#             */
-/*   Updated: 2026/04/14 15:30:26 by sihasima         ###   ########.fr       */
+/*   Updated: 2026/04/14 16:28:38 by sihasima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,18 @@ int main(int argc, char **argv)
 {
 	/*le parsing et autre...*/
 	t_config *config;
+	t_node 	**a;
 	t_node	**stack_a;
-	t_node	**stack_b;
+	t_node	*stack_b;
+	int		disorder;
 
+	if (!stack_a || !stack_b)
+		return (0);
+	//disorder = compute_disorder(stack_a);
+	stack_a = ft_stack(argc, argv, a);
 	init_config(config);
 	if (config->strat = STRAT_ADAPTIVE)
-		ft_adaptive(*stack_a, config);
+		ft_adaptive(stack_a, config);
 	else if (config->strat = STRAT_SIMPLE)
 		ft_short_simple(stack_a, stack_b, config);
 	else if (config->strat = STRAT_MEDIUM)
